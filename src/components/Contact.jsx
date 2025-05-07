5717e10a08902ee719ba9e0b542684a44d33b7a3
kamal Prajapat <kamalprajapat7117@gmail.com>
2025-05-08 00:42:33 +0530

    update code

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -140,7 +140,7 @@ const Contact = () => {
     e.preventDefault();
 
     try {
-      const res = await fetch("https://sa-club-backend.vercel.app/contact", {
+      const res = await fetch("https://sa-club-backend.vercel.app/api/contact", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
